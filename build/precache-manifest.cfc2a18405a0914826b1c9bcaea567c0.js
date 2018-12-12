00df8f54b355a0c2b9440c5390e5e673454eabdc
yangjianxin <150018405@qq.com>
2018-12-12 10:48:36 +0800

    部署目录

---
+++ build/precache-manifest.cfc2a18405a0914826b1c9bcaea567c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "879a4bf9fb9e201b36f4",
+    "url": "/react-app/static/js/main.879a4bf9.chunk.js"
+  },
+  {
+    "revision": "25b99e82e99a9e488ebb",
+    "url": "/react-app/static/js/1.25b99e82.chunk.js"
+  },
+  {
+    "revision": "879a4bf9fb9e201b36f4",
+    "url": "/react-app/static/css/main.14417b62.chunk.css"
+  },
+  {
+    "revision": "25b99e82e99a9e488ebb",
+    "url": "/react-app/static/css/1.d1294ac9.chunk.css"
+  },
+  {
+    "revision": "72e1c6d1d9b3337e5efdbe8ffaec93f2",
+    "url": "/react-app/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80d0296ce8fd4d72905c48ccc5f77d9f.js"
+  "/react-app/precache-manifest.cfc2a18405a0914826b1c9bcaea567c0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
